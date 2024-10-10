4e83da86509516cc2a8e9649ad97c7ab6a07de6c
Khoa Nguyen <khoa@chitmeo.com>
2024-10-10 17:58:07 +0700

    Refactor GetPostsWithPaginationQuery to return a PaginatedList<PostDto>

--- src/plugins/Dev.Plugin.Blog/Application/UseCases/Posts/Queries/GetPostsWithPagination.cs
+++ src/plugins/Dev.Plugin.Blog/Application/UseCases/Posts/Queries/GetPostsWithPagination.cs
@@ -1,7 +1,37 @@
-﻿using MediatR;
+﻿using Dev.Common;
+using Dev.Common.Mappings;
+using MediatR;
 
 namespace Dev.Plugin.Blog.Application.UseCases.Posts.Queries;
 
-internal class GetPostsWithPaginationQuery : IRequest<PostDto>
+internal class GetPostsWithPaginationQuery : IRequest<PaginatedList<PostDto>>
 {
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
+}
+internal class GetPostsWithPaginationQueryHandler : IRequestHandler<GetPostsWithPaginationQuery, PaginatedList<PostDto>>
+{
+    private readonly IBlogDbContext _context;
+
+    public GetPostsWithPaginationQueryHandler(IBlogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<PaginatedList<PostDto>> Handle(GetPostsWithPaginationQuery request, CancellationToken cancellationToken)
+    {
+        var posts = await _context.Posts
+            .Where(p => p.IsPublished)
+            .OrderByDescending(p => p.CreatedAt)
+            .Skip((request.PageNumber - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .Select(p => new PostDto
+            {
+                Id = p.Id,
+                Title = p.Title
+            })
+            .PaginatedListAsync(request.PageNumber, request.PageSize);
+
+        return posts;
+    }
 }
